d00b990cda72ce7ae00985c009030dfb475508b5
codedlibra <adeola.ogundijo@yahoo.com>
2023-06-27 10:46:01 -0700

    program that prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of two two-digit number
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 <= 98; digit++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+		{
+			putchar((digit1 / 10) + '0');
+			putchar((digit1 % 10) + '0');
+			putchar(' ');
+			putchar((digit2 / 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 98 && digit2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -10,3 +10,4 @@
 9-print_comb.c contains a program that prints all possible combinations of single-digit numbers
 100-print_comb3.c ontains a program that prints all possible different combinations of two digits.
 101-print_comb4.c contains program that prints all possible different combinations of three digits.
+102-print_comb5.c contains program that prints all possible combinations of two two-digit numbers
