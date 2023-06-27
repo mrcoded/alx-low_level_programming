7389099dee2652cb0fb95026c166ec35d2070a79
codedlibra <adeola.ogundijo@yahoo.com>
2023-06-27 10:47:16 -0700

    program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 <= 98; digit++)
+	for (digit1 = 0; digit1 <= 98; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
 		{
