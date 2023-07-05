628e90ea76a2cbf9cc4f4926efabae62c62518ad
codedlibra <adeola.ogundijo@yahoo.com>
2023-07-05 15:40:49 -0700

    function that draws a diagonal line on the terminal.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,24 +5,22 @@
  */
 void print_diagonal(int n)
 {
-	int len, space;
+	int i, space;
 
-	if (n < 0)
+	if (n <= 0)
 	{
-		for (len = 0; len < n; len++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			for (space = 0; space < len; space++)
+			for (space = 0; space < i; space++)
 			{
 				_putchar(' ');
 			}
-			_putchar('\\');
-			
-			if (len == n - 1)
-			{
-				continue;
-			}
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,4 +6,5 @@
 5-more_numbers.c contains function that prints 10 times the numbers, from 0 to 14.
 6-print_line.c contains a function that draws a straight line in the terminal.
 7-print_diagonal.c contains function that draws a diagonal line on the terminal.
+8-print_square.c contains function that prints a square, followed by a new line.
 
