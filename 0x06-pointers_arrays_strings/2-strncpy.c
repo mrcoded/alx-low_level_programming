4dda13dcaca51fea542917c10b398f6937214068
codedlibra <adeola.ogundijo@yahoo.com>
2023-07-09 05:45:06 -0700

    function to copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - "Copies a string"
+ *
+ * @dest: "Points to the destination string"
+ * @src: "Points to the source string"
+ * @n: size of full string
+ *
+ * Return: "Copied string into dest"
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,4 +1,5 @@
 0-strcat.c contains afunction that concatenates two strings.
 1-strncat.c contains function that concatenates two strings with a n byte.
+2-strncpy.c contains a function to copy a string.
 
 
