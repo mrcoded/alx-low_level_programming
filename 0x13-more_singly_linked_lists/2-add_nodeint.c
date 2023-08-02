442aafe88e3c98d4279be34a98da27da5ae52174
codedlibra <adeola.ogundijo@yahoo.com>
2023-08-01 17:04:41 -0700

    function that adds a new node at the beginning of a listint_t list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_node - "Adds a new node at the beginning of a linked list"
+ * add_nodeint - "Adds a new node at the beginning of a linked list"
  *
  * @head: "Points to the address of the head of the linked list"
  * @n: "The integer for the new node to contain"
