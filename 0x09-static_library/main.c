11d0a4080a6f5f05815b4e013523df755308740e
codedlibra <adeola.ogundijo@yahoo.com>
2023-09-01 15:30:55 -0700

    libmy.a

---
+++ 0x09-static_library/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
+	_puts("- Kevin Mitnick");
+	return (0);
+}
